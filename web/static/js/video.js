6c175d4e2a732f3977eba3ac3f8f0e561ebd6b32
Alex Buschle <alexwbuschle@gmail.com>
2016-06-24 14:33:29 -0300

    Filtering annotations

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -31,11 +31,14 @@ let Video = {
     })
 
     vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
       this.renderAnnotation(msgContainer, resp)
     })
 
     vidChannel.join()
       .receive("ok", resp => {
+        let ids = resp.annotations.map(ann => ann.id)
+        if(ids > 0) { vidChannel.params.last_seen_id = Math.max(...ids) }
         this.scheduleMessages(msgContainer, resp.annotations)
       })
       .receive("error", reason => console.log("join failed", reason) )
